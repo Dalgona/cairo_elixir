a92af67130b62c88694897590fd406e58ed03de7
Dalgona <me@dalgona.dev>
2019-12-19 19:17:59 +0900

    Refactor NativeFunctions module with macro

--- lib/cairo/native_functions.ex
+++ lib/cairo/native_functions.ex
@@ -1,6 +1,9 @@
 defmodule Cairo.NativeFunctions do
   @moduledoc false
 
+  require __MODULE__.Macros
+  import __MODULE__.Macros
+
   @on_load :load_nif
 
   def load_nif do
@@ -14,31 +17,29 @@ defmodule Cairo.NativeFunctions do
   # SURFACES
   #
 
-  def surface_create_similar(surface, content, width, height), do: nif_err!
-  def surface_create_similar_image(surface, format, width, height), do: nif_err!
-  def surface_create_for_rectangle(surface, x, y, width, height), do: nif_err!
-  def surface_get_content(_surface), do: nif_err!()
-  def surface_mark_dirty(surface), do: nif_err!
-  def surface_mark_dirty_rectangle(surface, x, y, width, height), do: nif_err!
-  def surface_set_device_offset(surface, x_offset, y_offset), do: nif_err!
-  def surface_get_device_offset(surface), do: nif_err!
-  def surface_set_device_scale(surface, x_scale, y_scale), do: nif_err!
-  def surface_get_device_scale(surface), do: nif_err!
-  def surface_set_fallback_resolution(surface, x_ppi, y_ppi), do: nif_err!
-  def surface_get_fallback_resolution(surface), do: nif_err!
+  defnif surface_create_similar(surface, content, width, height)
+  defnif surface_create_similar_image(surface, format, width, height)
+  defnif surface_create_for_rectangle(surface, x, y, width, height)
+  defnif surface_get_content(surface)
+  defnif surface_mark_dirty(surface)
+  defnif surface_mark_dirty_rectangle(surface, x, y, width, height)
+  defnif surface_set_device_offset(surface, x_offset, y_offset)
+  defnif surface_get_device_offset(surface)
+  defnif surface_set_device_scale(surface, x_scale, y_scale)
+  defnif surface_get_device_scale(surface)
+  defnif surface_set_fallback_resolution(surface, x_ppi, y_ppi)
+  defnif surface_get_fallback_resolution(surface)
 
   #
   # IMAGE SURFACES
   #
 
-  def format_stride_for_width(_format, _width), do: nif_err!()
-  def image_surface_create(_format, _width, _height), do: nif_err!()
-  def image_surface_create_for_data(_data, _format, _width, _height, _stride), do: nif_err!()
-  def image_surface_get_data(_surface), do: nif_err!()
-  def image_surface_get_format(_surface), do: nif_err!()
-  def image_surface_get_width(_surface), do: nif_err!()
-  def image_surface_get_height(_surface), do: nif_err!()
-  def image_surface_get_stride(_surface), do: nif_err!()
-
-  defp nif_err!, do: raise "nif not loaded"
+  defnif format_stride_for_width(format, width)
+  defnif image_surface_create(format, width, height)
+  defnif image_surface_create_for_data(data, format, width, height, stride)
+  defnif image_surface_get_data(surface)
+  defnif image_surface_get_format(surface)
+  defnif image_surface_get_width(surface)
+  defnif image_surface_get_height(surface)
+  defnif image_surface_get_stride(surface)
 end
