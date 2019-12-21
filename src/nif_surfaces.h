ea2f6ae7df485812c79c55e64604df46b4f440da
Dalgona <me@dalgona.dev>
2019-12-21 17:35:16 +0900

    Separate codes for surfaces functions

---
+++ src/nif_surfaces.h
@@ -0,0 +1,14 @@
+#include "nif_common.h"
+
+NIF_DECL(nif_surface_create_similar);
+NIF_DECL(nif_surface_create_similar_image);
+NIF_DECL(nif_surface_create_for_rectangle);
+NIF_DECL(nif_surface_get_content);
+NIF_DECL(nif_surface_mark_dirty);
+NIF_DECL(nif_surface_mark_dirty_rectangle);
+NIF_DECL(nif_surface_set_device_offset);
+NIF_DECL(nif_surface_get_device_offset);
+NIF_DECL(nif_surface_set_device_scale);
+NIF_DECL(nif_surface_get_device_scale);
+NIF_DECL(nif_surface_set_fallback_resolution);
+NIF_DECL(nif_surface_get_fallback_resolution);
