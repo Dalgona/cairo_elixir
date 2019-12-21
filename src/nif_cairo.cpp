b66b6046b7ec4f7c0ef88bc6e538c1fde1f64839
Dalgona <me@dalgona.dev>
2019-12-21 17:41:34 +0900

    Refactor nif_funcs array using a macro

--- src/nif_cairo.cpp
+++ src/nif_cairo.cpp
@@ -34,28 +34,30 @@ int load(ErlNifEnv *env, void **priv, ERL_NIF_TERM load_info)
   return 0;
 }
 
-ErlNifFunc nif_funcs[] = {
-  { "surface_create_similar", 4, nif_surface_create_similar, 0 },
-  { "surface_create_similar_image", 4, nif_surface_create_similar_image, 0 },
-  { "surface_create_for_rectangle", 5, nif_surface_create_for_rectangle, 0 },
-  { "surface_get_content", 1, nif_surface_get_content, 0 },
-  { "surface_mark_dirty", 1, nif_surface_mark_dirty, 0 },
-  { "surface_mark_dirty_rectangle", 5, nif_surface_mark_dirty_rectangle, 0 },
-  { "surface_set_device_offset", 3, nif_surface_set_device_offset, 0 },
-  { "surface_get_device_offset", 1, nif_surface_get_device_offset, 0 },
-  { "surface_set_device_scale", 3, nif_surface_set_device_scale, 0 },
-  { "surface_get_device_scale", 1, nif_surface_get_device_scale, 0 },
-  { "surface_set_fallback_resolution", 3, nif_surface_set_fallback_resolution, 0 },
-  { "surface_get_fallback_resolution", 1, nif_surface_get_fallback_resolution, 0 },
+#define NIF(name, arity) { #name, arity, nif_##name, 0 }
 
-  { "format_stride_for_width", 2, nif_format_stride_for_width, 0 },
-  { "image_surface_create", 3, nif_image_surface_create, 0 },
-  { "image_surface_create_for_data", 5, nif_image_surface_create_for_data, 0},
-  { "image_surface_get_data", 1, nif_image_surface_get_data, 0 },
-  { "image_surface_get_format", 1, nif_image_surface_get_format, 0},
-  { "image_surface_get_width", 1, nif_image_surface_get_width, 0},
-  { "image_surface_get_height", 1, nif_image_surface_get_height, 0},
-  { "image_surface_get_stride", 1, nif_image_surface_get_stride, 0}
+ErlNifFunc nif_funcs[] = {
+  NIF(surface_create_similar, 4),
+  NIF(surface_create_similar_image, 4),
+  NIF(surface_create_for_rectangle, 5),
+  NIF(surface_get_content, 1),
+  NIF(surface_mark_dirty, 1),
+  NIF(surface_mark_dirty_rectangle, 5),
+  NIF(surface_set_device_offset, 3),
+  NIF(surface_get_device_offset, 1),
+  NIF(surface_set_device_scale, 3),
+  NIF(surface_get_device_scale, 1),
+  NIF(surface_set_fallback_resolution, 3),
+  NIF(surface_get_fallback_resolution, 1),
+
+  NIF(format_stride_for_width, 2),
+  NIF(image_surface_create, 3),
+  NIF(image_surface_create_for_data, 5),
+  NIF(image_surface_get_data, 1),
+  NIF(image_surface_get_format, 1),
+  NIF(image_surface_get_width, 1),
+  NIF(image_surface_get_height, 1),
+  NIF(image_surface_get_stride, 1),
 };
 
 ERL_NIF_INIT(Elixir.Cairo.NativeFunctions, nif_funcs, load, NULL, NULL, NULL)
