ed97ceb0368d0d07864ab074830426825bc08ff6
Dalgona <dalgona@hontou.moe>
2020-01-21 08:08:35 +0900

    Print debug messages when resource destructors are called

--- src/resource_types.cpp
+++ src/resource_types.cpp
@@ -1,14 +1,23 @@
 #define CAIRO_ELIXIR_NIF_RESOURCE_TYPES_IMPL
+#include "debug.h"
 #include "resource_types.h"
 
 template <> struct _destroy<cairo_t>
 {
-  static void call(cairo_t *obj) { cairo_destroy(obj); }
+  static void call(cairo_t *obj)
+  {
+    dbgprintf("\x1b[90mDestroying cairo_t @ %p, ref count: %d\x1b[m\r\n", obj, cairo_get_reference_count(obj));
+    cairo_destroy(obj);
+  }
 };
 
 template <> struct _destroy<cairo_surface_t>
 {
-  static void call(cairo_surface_t *obj) { cairo_surface_destroy(obj); }
+  static void call(cairo_surface_t *obj)
+  {
+    dbgprintf("\x1b[90mDestroying cairo_surface_t @ %p, ref count: %d\x1b[m\r\n", obj, cairo_surface_get_reference_count(obj));
+    cairo_surface_destroy(obj);
+  }
 };
 
 template void resource_dtor<cairo_t>(ErlNifEnv *env, void *obj);
