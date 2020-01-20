431af7ce6c01fc2fcfbf02f5d4870efd264121a2
Dalgona <dalgona@hontou.moe>
2020-01-20 21:35:14 +0900

    Define vec2_t type and add get_vec2 function

--- src/utils.cpp
+++ src/utils.cpp
@@ -20,3 +20,29 @@ int get_number(ErlNifEnv *env, const ERL_NIF_TERM term, double *dest)
     return 0;
   }
 }
+
+int get_vec2(ErlNifEnv *env, const ERL_NIF_TERM term, vec2_t *dest)
+{
+  const ERL_NIF_TERM *arr;
+  int arity;
+
+  if (!enif_get_tuple(env, term, &arity, &arr))
+  {
+    return 0;
+  }
+
+  if (arity != 2)
+  {
+    return 0;
+  }
+
+  if (get_number(env, arr[0], &std::get<0>(*dest))
+      && get_number(env, arr[1], &std::get<1>(*dest)))
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,7 @@
 #ifndef CAIRO_ELIXIR_NIF_UTILS_H
 #define CAIRO_ELIXIR_NIF_UTILS_H
 
+#include <utility>
 #include <erl_nif.h>
 
 #ifndef CAIRO_ELIXIR_NIF_UTILS_IMPL
@@ -10,6 +11,9 @@
 #define EXTERN
 #endif
 
+using vec2_t = std::pair<double, double>;
+
 EXTERN int get_number(ErlNifEnv *env, const ERL_NIF_TERM term, double *dest);
+EXTERN int get_vec2(ErlNifEnv *env, const ERL_NIF_TERM term, vec2_t *dest);
 
 #endif
