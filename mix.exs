4a61fae635cb71115719afcdd2c87a5ae5f98c2e
Dalgona <me@dalgona.dev>
2021-04-24 17:48:16 +0900

    build: change elixir version requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cairo.MixProject do
     [
       app: :cairo,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: ">= 1.8.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
