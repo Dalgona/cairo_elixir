2502d0fea874acb443354b2975947ae23eb39caf
Dalgona <me@dalgona.dev>
2021-04-25 17:23:15 +0900

    feat: define types for wrap/ellipsize mode

---
+++ lib/cairo/pango.ex
@@ -0,0 +1,12 @@
+defmodule Cairo.Pango do
+  @type wrap_mode ::
+          :wrap_word
+          | :wrap_char
+          | :wrap_word_char
+
+  @type ellipsize_mode ::
+          :ellipsize_none
+          | :ellipsize_start
+          | :ellipsize_middle
+          | :ellipsize_end
+end
