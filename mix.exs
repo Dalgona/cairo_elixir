6eef379148e4c951ef44a67dd219403529d1a71d
Dalgona <me@dalgona.dev>
2021-04-26 00:05:54 +0900

    build: use elixir_make Mix compiler

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Cairo.MixProject do
       version: "0.1.0",
       elixir: ">= 1.8.0",
       start_permanent: Mix.env() == :prod,
+      compilers: [:elixir_make] ++ Mix.compilers(),
       deps: deps()
     ]
   end
@@ -22,8 +23,7 @@ defmodule Cairo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:elixir_make, "0.6.2"}
     ]
   end
 end
