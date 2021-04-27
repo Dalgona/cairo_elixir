a547116addd06267def031158ce195655b84687e
Dalgona <me@dalgona.dev>
2021-04-28 00:20:34 +0900

    refactor: remove REQUIRE_OBJECT macro

--- src/include/macros.h
+++ src/include/macros.h
@@ -5,15 +5,4 @@
 #define NIF_DECL(name) ERL_NIF_TERM name(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 #define ENSURE_ARGC(x) if (argc != x) { return enif_make_badarg(env); }
 
-#define REQUIRE_OBJECT(T, TRes, var, argi) \
-  T *var = nullptr; \
-  { \
-    T **_ppobj = nullptr; \
-    if (!enif_get_resource(env, argv[argi], nif_resource<T>::type, (void **)&_ppobj)) \
-    { \
-      return enif_make_badarg(env); \
-    } \
-    var = *_ppobj; \
-  }
-
 #endif
